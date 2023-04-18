120dcd6c7f6150b1023e1c19e605d5e42a0d7d74
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2023-04-19 01:17:17 +0530

    Dark Mode Theme in Home Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,22 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.scss';
 import loginImg from '../../assets/login.svg';
+import { useTheme } from '@mui/material';
+import { tokens } from '../../theme';
 
 const Home = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <>
       {/* HERO SECTION */}
-      <section className='container hero'>
+      <section
+        className='container hero'
+        style={{ backgroundColor: `${colors.primary[500]}` }}
+      >
         <div className='hero-text'>
-          <h1>ezCompliance Software Solution</h1>
-          <p>ISO Compliance System (ICS)</p>
-          <p>
+          <h1 style={{ color: `${colors.grey[100]}` }}>
+            ezCompliance Software Solution
+          </h1>
+          <p style={{ color: `${colors.grey[100]}` }}>
+            ISO Compliance System (ICS)
+          </p>
+          <p style={{ color: `${colors.grey[100]}` }}>
             A management system standard which sets out the requirements and
             provides guidelines for establishing, developing, implementing,
             evaluating, maintaining, and continually improving a compliance
             management system (CMS).
           </p>
-          <p>
+          <p style={{ color: `${colors.grey[100]}` }}>
             The International Organization for Standardization (ISO) is an
             international organization that sets worldwide proprietary,
             commercial, and industry standards. ISO has issued thousands of
