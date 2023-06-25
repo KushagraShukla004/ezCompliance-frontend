8a3feaa3ce5842c6bf0dfb94559e17e089a4a8b7
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2023-06-25 21:29:34 +0530

    Home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Home.scss';
-import loginImg from '../../assets/login.svg';
-import { useTheme } from '@mui/material';
-import { tokens } from '../../theme';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.scss";
+import loginImg from "../../assets/login.svg";
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
 
 const Home = () => {
   const theme = useTheme();
@@ -12,10 +12,10 @@ const Home = () => {
     <>
       {/* HERO SECTION */}
       <section
-        className='container hero'
+        className="container hero"
         style={{ backgroundColor: `${colors.primary[500]}` }}
       >
-        <div className='hero-text'>
+        <div className="hero-text">
           <h1 style={{ color: `${colors.grey[100]}` }}>
             ezCompliance Software Solution
           </h1>
@@ -28,34 +28,18 @@ const Home = () => {
             evaluating, maintaining, and continually improving a compliance
             management system (CMS).
           </p>
-          <p style={{ color: `${colors.grey[100]}` }}>
-            The International Organization for Standardization (ISO) is an
-            international organization that sets worldwide proprietary,
-            commercial, and industry standards. ISO has issued thousands of
-            standards aimed to support organizations in delivering products and
-            services that are of higher quality, safer, more secure, and
-            environmentally friendly. ISO standards are important because they
-            provide organizations and their customers with a shared benchmark of
-            quality and safety of processes, services, products. Well-known ISO
-            standards include ISO 9001 for quality management systems, ISO 20000
-            for IT service management systems, ISO 27001 for IT security
-            management systems, ISO 14001 for environmental management systems,
-            and ISO 45001 for occupational health and safety. While not required
-            by law for all industries, ISO certifications are highly regarded
-            and internationally recognized.
-          </p>
-          <div className='hero-buttons --flex-start'>
-            <button className='--btn --btn-danger'>
-              <Link to={'/register'}>Register</Link>
+          <div className="hero-buttons --flex-start">
+            <button className="--btn --btn-danger">
+              <Link to={"/register"}>Register</Link>
             </button>
-            <button className='--btn --btn-primary2'>
-              <Link to={'/login'}>Login</Link>
+            <button className="--btn --btn-primary2">
+              <Link to={"/login"}>Login</Link>
             </button>
           </div>
         </div>
 
-        <div className='hero-image'>
-          <img src={loginImg} alt='Inventory' />
+        <div className="hero-image">
+          <img src={loginImg} alt="Inventory" />
         </div>
       </section>
     </>
